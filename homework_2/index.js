b06870b06f4565bca3302196482e86a1b4dcf782
Andrej <a.arsenovski@labdigital.nl>
2024-05-10 22:39:11 +0200

    fix: fix last exercise

--- homework_2/index.js
+++ homework_2/index.js
@@ -203,14 +203,15 @@ if (temp > 30) {
 // exercise 5
 
 let divisionWithNumbers = (num) => {
-  if (num % 3 === 0) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 === 0) {
     console.log("Fizz");
-    if (num % 5 === 0) {
-      console.log("FizzBuzz");
-    }
   } else if (num % 5 === 0) {
     console.log("Buzz");
   }
 };
 
+divisionWithNumbers(5);
+divisionWithNumbers(9);
 divisionWithNumbers(15);
